c3ace02223bfddc63a045b55590f159dd2e233d3
Evans Pauliuts <pauliutsevans@gmail.com>
2019-08-28 15:33:28 +0300

    add rest framework apis

--- config/settings.py
+++ config/settings.py
@@ -38,9 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd-party apps
+    'rest_framework',  # new
+
     # local app
     'posts.apps.PostsConfig',
 ]
+
+# new
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)  # new
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,)  # new
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
